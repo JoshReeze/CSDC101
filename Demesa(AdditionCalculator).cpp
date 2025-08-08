419f2db758f48771aa844b3b58137ece830ed1d1
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-08-08 14:35:34 +0800

    Create Demesa(AdditionCalculator).cpp

---
+++ Demesa(AdditionCalculator).cpp
@@ -0,0 +1,16 @@
+#include <iostream>// input and output library
+using namespace std;// standard namespace for sin and cout
+
+int main()
+{
+   int num1,num2,sum;// integer variables
+
+   cout<<"Enter a number: ";//output enter a number
+   cin>>num1;//user input number1
+   cout<<"Enter another number: ";//output enter another number 
+   cin>>num2;//user input number2
+   sum=num1+num2;//set the value of sum as number1 and number2
+   cout<<"The sum of the two number is: " <<sum;//output sum of numbers
+ 
+    return 0;
+}
