3db6bad01e9ef714ea0955565655100c68c9adb4
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-08-08 14:11:37 +0800

    Create Demesa.cpp
    Added main code about simple calculator

---
+++ Demesa.cpp
@@ -0,0 +1,16 @@
+#include <iostream>// input and output library
+using namespace std;// standard namespace for sin and cout
+
+int main()
+{
+   int num1,num2,product;// integer variables
+
+   cout<<"Enter a number: ";//output enter a number
+   cin>>num1;//user input number1
+   cout<<"Enter another number: ";//output enter another number 
+   cin>>num2;//user input number2
+   product=num1*num2;//set the value of product as number1 and number2
+   cout<<"The sum of the two number is: " <<product;//output product of numbers
+ 
+    return 0;
+}
