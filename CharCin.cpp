38ac5c23074c89a6db4f432facdb6c9af125c177
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-08-29 14:32:23 +0800

    CharCin.cpp

---
+++ CharCin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>// input and output library
+using namespace std;// standard namespace for sin and cout
+
+int main()
+{
+   char decision;
+    
+   cin >> decision;
+   cout << decision << " is the correct answer";
+  
+ 
+    return 0;
+}
