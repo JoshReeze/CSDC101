d5294026966db6869a75190de859ed6e12df545c
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-08-29 14:40:40 +0800

    String.cpp

---
+++ String.cpp
@@ -0,0 +1,14 @@
+#include <iostream>// input and output library
+using namespace std;// standard namespace for sin and cout
+
+int main()
+{
+    string blackpink, twice;
+    
+    blackpink = "Lisa Manoban ";
+    twice = "Park Jihyo";
+    
+        cout << blackpink + twice << " is my bias\n";
+   
+    return 0;
+}
