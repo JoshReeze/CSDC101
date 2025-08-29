79821eff1349da80fa4836a117686c795b6020f6
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-08-29 14:31:48 +0800

    boolTorF.cpp

---
+++ boolTorF.cpp
@@ -0,0 +1,18 @@
+#include <iostream>// input and output library
+using namespace std;// standard namespace for sin and cout
+
+int main()
+{
+    bool decision;
+    
+    decision = true;
+    
+    if(decision)
+    {
+        cout << "the statement is true\n";
+    }
+    else 
+        cout << "the answer is false\n";
+
+    return 0;
+}
