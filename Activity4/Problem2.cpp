b27fce6e12c2bced77a4067210d809148266969b
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-10-02 22:43:21 -0700

    Problem2.cpp

---
+++ Activity4/Problem2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    double temperature;
+   
+    cout << "Enter today's temperature in Celsius: ";
+    cin >> temperature;
+
+    if (temperature >= 30) {
+        cout << "It's hot! Lemonade costs $0.80 today." << endl;
+    } 
+    
+    else {
+        cout << "Lemonade costs $1.00 today." << endl;
+    }
+
+    return 0;
+}
