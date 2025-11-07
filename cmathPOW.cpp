e4791e57f6b0367106d5177d977ce42a8e822470
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-11-06 20:20:04 -0800

    cmathPOW.cpp

---
+++ cmathPOW.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    
+    int n = 3, exponent = 2;
+    
+    cout << n << " raise to " << exponent << " is equals to " << pow(n, exponent) << endl;
+    
+    return 0;
+}
