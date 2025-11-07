a9132a74080d43888d0a1185aeb068bf30755036
JoshReeze <jrdemesa@gbox.adnu.edu.ph>
2025-11-06 20:28:36 -0800

    cmathROUND.cpp

---
+++ cmathROUND.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    
+    float n = 6.5;
+    
+    cout << "round to te nearest integer " << round(n) << endl;
+    cout << "round up " << ceil(n) << endl;
+    cout << "round down " << floor(n) << endl;
+    cout << "The value of n is equals to " << n << endl;
+    
+    return 0;
+}
